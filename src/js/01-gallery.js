f045ad83e92ed12ada4c5d41c7ca4bd55938c38f
shuro4ka <zzlesia77@gmail.com>
2022-06-12 23:28:51 +0100

    add player

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
 
-console.log(galleryItems);
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
+
+// Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+
+const imagesMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+galleryContainer.addEventListener('click', onImageClick);
+
+createGalleryMarkup(galleryItems)
+
+function createGalleryMarkup (galleryItems){
+  return galleryItems
+      .map(({preview, description, original}) => {
+        return `
+        <a class="gallery__item" 
+        href="${original}">
+          <img class="gallery__image"
+          src="${preview}" 
+          alt="${description}" />
+        </a>`
+      })
+      .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+
+function onImageClick(event) {
+  event.preventDefault();
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,8 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', function() {
+    console.log('played the video!');
+});
