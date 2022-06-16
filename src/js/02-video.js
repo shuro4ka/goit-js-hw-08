4890084f7089ca760951c3ed39aee67cfcebc021
shuro4ka <zzlesia77@gmail.com>
2022-06-16 08:07:55 +0100

    add changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,31 @@
 import Player from '@vimeo/player';
+import _ from "lodash";
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const SECONDS = localStorage.getItem(LOCALSTORAGE_KEY);
 
-player.on('timeupdate', function() {
-    console.log('played the video!');
-});
+if(SECONDS) {
+    const SECONDS_PARSED = JSON.parse(SECONDS);
+    player.setCurrentTime(SECONDS_PARSED)
+        .then(seconds => {
+            seconds = SECONDS_PARSED;
+        })
+        
+        .catch(error => {
+            console.error(error);
+        })
+}
+
+player.on('timeupdate', _.throttle(() => {
+    player.getCurrentTime()
+        .then(seconds => {
+            
+            localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+        })
+        
+        .catch(error => {
+            console.error(error);
+        })
+}, 3000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import _ from "lodash";
+const LOCALSTORAGE_KEY = "feedback-form-state";
+const form = document.querySelector(".feedback-form");
+const persistantFormState = localStorage.getItem(LOCALSTORAGE_KEY);
 
+if(persistantFormState) {
+    const FORM_DATA =  JSON.parse(persistantFormState);
+    form.email.value = FORM_DATA.email;
+    form.message.value = FORM_DATA.message;
+}
+
+form.addEventListener( "input", _.throttle((event) => {
+    onInputSave(event);
+}, 500));
+
+form.addEventListener('submit', onSubmit);
+
+function onInputSave(event) {
+    event.preventDefault();
+    const DataObject = getFormData(form);
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(DataObject));
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+
+    const DataObject = getFormData(form);
+
+    if(DataObject.email !== '' || DataObject.message !== '') {
+        console.log(DataObject);
+    }
+
+    event.target.email.value = '';
+    event.target.message.value = '';
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function getFormData(form) {
+    const {
+        elements: { email, message }
+    } = form;
+
+    return {
+        email: email.value,
+        message: message.value
+    }
+}
